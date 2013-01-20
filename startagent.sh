4f0c32b33164dddd9d9b2e92cae8c822b621ce6b
Sam <Sam@David-Mauldins-MacBook-Pro.local>
2013-01-20 16:06:49 -0600

    Update

--- startagent.sh
+++ startagent.sh
@@ -1,5 +1,5 @@
 echo "Updating local copy."
-echo "This may take awhile if it is your first time or haven't used it yet."
+echo "This may take awhile if it is your first time or haven't used it in awhile."
 cd remotesupport
 git pull
 cd node-rcon/lib
